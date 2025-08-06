1723f5ec69c2cff870216cb710795a152d283153
PADA1-2-15 <pada1-2-15@Erxess-iMac.local>
2025-08-06 15:23:29 +0800

    calculator

--- od.js
+++
@@ -1,9 +0,0 @@
-function printPyramid(totalLines){
-    for (let i=1; i<totalLines; i++) {
-        let spaces = " ".repeat(totalLines - i);
-        let stars = "*".repeat(2 * i-1);
-        console.log(spaces + stars);
-    }
-}
-
-printPyramid(20);
